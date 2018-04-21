a51a47b9477d7d7c84dcc6092a90677764e944bd
Nguyen Ho <hothiennguyen124@gmail.com>
2018-04-21 13:39:22 +0700

    Add new route

---
+++ components/route.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Router, Scene, Actions, Modal } from 'react-native-router-flux'
+import Rules from './rules'
+import Login from './login'
+import SetUp from './setup/container'
+import Game from './games/container'
+import Assigning from './assigning/container'
+const Routes = {
+  Game: { screen: Game },
+  SetUp: {
+    screen: SetUp,
+   },
+  Assigning: { screen: Assigning }
+}
+export default Routes;
---
+++ components/setup/actionCreator.js
@@ -0,0 +1,104 @@
+export const validateRules = () => (dispatch, getState) => {
+  const rules = getState().rules;
+  if (rules.playerNumber != rules.playerNames.split(',').length) {
+    return { valid: false, message: `Player names don't match player number` }
+  }
+  if (checkDuplicate(rules.playerNames.split(','))) {
+    return { valid: false, message: `Players have duplicate name`}
+  }
+  return { valid: true, message: 'Validation success'}
+}
+
+export const randomRoles = () => (dispatch, getState) => {
+  const rules = getState().rules;
+
+  const extendRoles = rules.roles.filter(role => role.active).map(role => role.name)
+  const wereWolfNo = Math.floor(rules.playerNumber / 3)
+  const villagerNo = rules.playerNumber - wereWolfNo - extendRoles.length
+
+  let wereWolf = []
+  for(let i = 0; i < wereWolfNo; i++) { wereWolf.push('Werewolf')}
+
+  let villagers = []
+  for(let i = 0; i < villagerNo; i++) { villagers.push('Villager')}
+
+  const allRoles =  wereWolf.concat(villagers, extendRoles)
+  let randomArray = []
+  for(let i = 0; i < rules.playerNumber; i++) { randomArray.push(i)}
+  randomArray = shuffle(randomArray)
+
+  let players = rules.playerNames.split(',')
+  // for(let i = 0; i < players.length; i++) { players[i].role = allRoles[randomArray[i]]}
+  players = players.map((player, index) => {
+    return {
+      name: player,
+      role: allRoles[randomArray[index]]
+    }
+  })
+  const callOrder = ['Werewolf'].concat(extendRoles)
+  dispatch({type: 'INIT_ASSIGN_STATE', payload: players})
+  dispatch({type: 'INIT_CALL_ORDER', payload: callOrder})
+}
+
+export const startGame = () => (dispatch, getState) => {
+  const rules = getState().rules;
+  const currentDay = 1;
+  const currentShift = 1;
+  const dayOne = {
+    day: 1,
+    shift: 0,
+    survivorsAmount: rules.playerNumber,
+    survivors: []
+  }
+}
+
+export const updatePlayerNumber = (playerNumber) => (dispatch, getState) => {
+  dispatch({
+    type: 'UPDATE_PLAYER_NUMBER',
+    payload: playerNumber
+  })
+}
+
+export const updatePlayerNames  = (playerNames) => (dispatch, getState) => {
+  dispatch({
+    type: 'UPDATE_PLAYER_NAMES',
+    payload: playerNames
+  })
+}
+
+export const updateRoles = (roles) => (dispatch, getState) => {
+  dispatch({
+    type: 'UPDATE_PLAYER_ROLES',
+    payload: roles
+  })
+}
+
+const checkDuplicate = (list) => {
+  const tempList = []
+  for(let i = 0; i < list.length; i++) {
+    if(tempList.indexOf(list[i]) > -1) {
+      return true
+    }
+    tempList.push(list[i]);
+  }
+  return false
+}
+
+const shuffle = (array) => {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
